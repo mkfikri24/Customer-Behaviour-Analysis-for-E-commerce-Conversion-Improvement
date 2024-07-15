866cec509e32f3eb684a1ef2e1e46dc1d780300c
Muhammad Khusni Fikri <113235525+mkfikri24@users.noreply.github.com>
2024-07-15 12:30:48 +0700

    Create Conversion Rate of Paid Order.sql

---
+++ SQL/Conversion Rate of Paid Order.sql
@@ -0,0 +1,32 @@
+SELECT
+  (
+  SELECT
+    COUNT(DISTINCT masked_user_id)
+  FROM
+    RPEcommerce.database.activity
+  WHERE
+    is_make_order IS TRUE
+    AND pilih_metode_pembayaran_count !=0
+    AND view_pdp_count !=0) AS total_complete_transaction,
+  (
+  SELECT
+    COUNT(DISTINCT masked_user_id)
+  FROM
+    RPEcommerce.database.activity
+  WHERE
+    session_start_global_count >=1) AS total_visitor,
+  (
+  SELECT
+    COUNT(DISTINCT masked_user_id)
+  FROM
+    RPEcommerce.database.activity
+  WHERE
+    is_make_order IS TRUE
+    AND pilih_metode_pembayaran_count !=0
+    AND view_pdp_count !=0) / (
+  SELECT
+    COUNT(DISTINCT masked_user_id)
+  FROM
+    RPEcommerce.database.activity
+  WHERE
+    session_start_global_count >=1) * 100 AS conversion_rate
