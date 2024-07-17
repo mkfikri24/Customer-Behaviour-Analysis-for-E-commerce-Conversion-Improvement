0bf701442e9cec7e6f43f9391f680fbf0b1dbc15
Muhammad Khusni Fikri <113235525+mkfikri24@users.noreply.github.com>
2024-07-17 11:41:02 +0700

    Update Conversion Rate of Paid Order.sql

--- SQL/Conversion Rate of Paid Order.sql
+++ SQL/Conversion Rate of Paid Order.sql
@@ -7,7 +7,9 @@ SELECT
   WHERE
     is_make_order IS TRUE
     AND pilih_metode_pembayaran_count !=0
-    AND view_pdp_count !=0) AS total_complete_transaction,
+    AND view_pdp_count !=0
+    AND session_start_global_count >= 1
+    AND view_microsite_count != 0) AS total_complete_transaction,
   (
   SELECT
     COUNT(DISTINCT masked_user_id)
@@ -23,7 +25,9 @@ SELECT
   WHERE
     is_make_order IS TRUE
     AND pilih_metode_pembayaran_count !=0
-    AND view_pdp_count !=0) / (
+    AND view_pdp_count !=0
+    AND session_start_global_count >= 1
+    AND view_microsite_count != 0) / (
   SELECT
     COUNT(DISTINCT masked_user_id)
   FROM
